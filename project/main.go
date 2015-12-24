eb6aadb75d07fe178a77662035213d578a3238d1
Prajakta Morale <prajaktamorale@gmail.com>
2015-12-23 23:05:54 -0800

    Project commit
    Signed-off-by: Prajakta Morale <prajaktamorale@gmail.com>

---
+++ project/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"flag"
+	"go/build"
+	"log"
+	"net/http"
+	"path/filepath"
+	"text/template"
+)
+
+var (
+	addr      = flag.String("addr", ":8080", "http service address")
+	assets    = flag.String("assets", defaultAssetPath(), "path to assets")
+	homeTempl *template.Template
+)
+
+func defaultAssetPath() string {
+	p, err := build.Default.Import("https://github.com/prajaktamorale/csci191/project", "", build.FindOnly)
+	if err != nil {
+		return "."
+	}
+	return p.Dir
+}
+
+func homeHandler(c http.ResponseWriter, req *http.Request) {
+	homeTempl.Execute(c, req.Host)
+}
+
+func main() {
+	flag.Parse()
+	homeTempl = template.Must(template.ParseFiles(filepath.Join(*assets, "home.html")))
+	h := newHub()
+	go h.run()
+	http.HandleFunc("/", homeHandler)
+	http.Handle("/ws", wsHandler{h: h})
+	if err := http.ListenAndServe(*addr, nil); err != nil {
+		log.Fatal("ListenAndServe:", err)
+	}
+}
+
